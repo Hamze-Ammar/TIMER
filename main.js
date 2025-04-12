14ac25468b1b9831bab1e99fc9a3a47181ceb023
Hamze Ammar <hamze.ammar@nsquared.io>
2025-04-12 16:41:07 -0400

    Fix date formatting in getFormattedEndsAt function to improve readability

--- main.js
+++ main.js
@@ -282,7 +282,7 @@ function timerApp() {
       const minutes = String(endDate.getMinutes()).padStart(2, '0');
       const seconds = String(endDate.getSeconds()).padStart(2, '0');
     
-      return `${month}, ${day} ${hours}:${minutes}:${seconds}`;
+      return `${month} ${day}, ${hours}:${minutes}:${seconds}`;
     },
 
     pauseTimer(index) {
